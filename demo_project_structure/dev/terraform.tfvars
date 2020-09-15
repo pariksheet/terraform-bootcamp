239e0255f07e7acf5c7af34515aca93b8750cffe
pariksheet <pari@Everiss-MacBook-Pro.local>
2020-09-15 11:06:36 +0200

    Use tfvars

---
+++ demo_project_structure/dev/terraform.tfvars
@@ -0,0 +1,2 @@
+subnet_index=33
+nginx_instance_type="t2.micro"
--- demo_project_structure/dev/vars.tf
+++ demo_project_structure/dev/vars.tf
@@ -1,9 +1,6 @@
 variable "subnet_index" {
   type = number
   description = "subnet index in 10.{}.0.0"
-  default = 11
 }
 
-variable "nginx_instance_type" {
-  default = "t2.micro"
-}
+variable "nginx_instance_type" {}
